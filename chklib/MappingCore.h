45b212e26fa4bec6ebb68b9358dd7483ea54a497
Heinermann <aheinerm@gmail.com>
2020-01-12 00:47:23 -0800

    Added MemTrigger for chklib

--- chklib/MappingCore.h
+++ chklib/MappingCore.h
@@ -1,3 +1,15 @@
 #pragma once
 
 #include "./Chkdraft/MappingCoreLib/MappingCore.h"
+
+namespace Chk {
+#pragma pack(push, 1)
+
+  __declspec(align(1)) struct MemTrigger {
+    uint32_t prev;
+    uint32_t next;
+    Trigger trigger;
+  };
+
+#pragma pack(pop)
+}
