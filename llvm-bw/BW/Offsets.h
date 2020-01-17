8dec1aa5f17f644ad98f60d17ba2c67b043804bf
Heinermann <aheinerm@gmail.com>
2020-01-16 23:00:41 -0800

    Added offsets

--- llvm-bw/BW/Offsets.h
+++ llvm-bw/BW/Offsets.h
@@ -1,2 +1,21 @@
 #pragma once
 
+#include <cstdint>
+
+namespace llvmbw::Offsets {
+    /**
+      * Returns true if the given offset can be written to in Remastered.
+      */
+    bool CanWrite(uint32_t offset, uint32_t numBytes = 4);
+    
+    /**
+      * Returns true if the given offset can be read in Remastered.
+      */
+    bool CanRead(uint32_t offset, uint32_t numBytes = 4);
+
+    /**
+      * Returns true if reading this value ensures that players will
+      * not become desynced and drop from the game.
+      */
+    bool IsSync(uint32_t offset, uint32_t numBytes = 4);
+}
